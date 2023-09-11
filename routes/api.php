956cf9be3e1ab2d898887b9a4faca7b2a1c3ef9e
Gino804 <giacomo.p03@gmail.com>
2023-09-11 15:18:41 +0200

    Added api projects index

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/projects', function () {
-    $projects = Project::all();
-    return response()->json($projects);
-});
-
 Route::apiResource('projects', ProjectController::class);
